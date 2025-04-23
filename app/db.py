c5924f6c45cd1b3c35c4c5bd08fdeed335c0a895
Sunil Kapil <sunilkapil@MacBook-Air.local>
2025-04-23 16:02:33 +0530

    Moved test.db to data folder for writable access

--- app/db.py
+++ app/db.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./test.db"  # fallback
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./data/test.db"
+
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

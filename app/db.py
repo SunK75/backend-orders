b3b4c71b5989975aef9a796f5ca4394b2ac44eab
Sunil Kapil <sunilkapil@MacBook-Air.local>
2025-04-23 16:12:39 +0530

    Fix path: moved test.db to root level

--- app/db.py
+++ app/db.py
@@ -5,9 +5,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./data/test.db"
-
+DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:///./test.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if "sqlite" in DATABASE_URL else {})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
